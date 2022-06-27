913dd9f312c1d3a3d54f3016819c427eecb46a0d
Leonardo Custodio <leonardo@custodio.me>
2022-06-27 16:35:37 -0300

    Add chainInfo processor (#3)
    add chaininfo

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { ProcessorConfig } from './types/custom/processorConfig'
 const config: ProcessorConfig = {
     chainName: 'rocfinity',
     prefix: 195,
-    // genesisHash: '0x1cb2120b3afd6da2dca23d8b95e30fd7eabe4357c3d470bc02487a7d6fd00d1d',
+    genesisHash: '0x1cb2120b3afd6da2dca23d8b95e30fd7eabe4357c3d470bc02487a7d6fd00d1d',
     dataSource: {
         archive: 'http://archive_gateway:8000/graphql',
         chain: 'wss://archive.rpc.rococo.efinity.io',
