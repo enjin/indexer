ea3ac92507cc79c5b223adc40a4a4a5dfbce4f4d
Leonardo Custodio <leonardo@custodio.me>
2022-06-27 15:18:22 -0300

    docker changes

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config: ProcessorConfig = {
     prefix: 195,
     // genesisHash: '0x1cb2120b3afd6da2dca23d8b95e30fd7eabe4357c3d470bc02487a7d6fd00d1d',
     dataSource: {
-        archive: 'http://localhost:12093/graphql',
+        archive: 'http://archive_gateway:8000/graphql',
         chain: 'wss://archive.rpc.rococo.efinity.io',
     },
     batchSize: 1,
