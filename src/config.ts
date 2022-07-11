84ba50756b1cbb45576fd992089413c0357c7312
Leonardo Custodio <leonardo@custodio.me>
2022-07-11 18:13:38 -0300

    Multi-chain envs (#14)
    configs multichain

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import { ProcessorConfig } from './types/custom/processorConfig'
 
 const config: ProcessorConfig = {
-    chainName: 'efinity',
-    prefix: 1110,
-    genesisHash: '0x335369975fced3fc22e23498da306a712f4fd964c957364d53c49cea9db8bc2f',
-    rpc: 'wss://rpc.efinity.io',
+    chainName: process.env.CHAIN_NAME || 'efinity',
+    prefix: process.env.CHAIN_PREFIX ? parseInt(process.env.CHAIN_PREFIX) : 1110,
+    genesisHash: process.env.GENESIS_HASH || '0x335369975fced3fc22e23498da306a712f4fd964c957364d53c49cea9db8bc2f',
+    rpc: process.env.CHAIN_RPC || 'wss://rpc.efinity.io',
     dataSource: {
         archive: 'http://archive_gateway:8000/graphql',
-        chain: 'wss://archive.rpc.efinity.io',
+        chain: process.env.CHAIN_ENDPOINT || 'wss://archive.rpc.efinity.io',
     },
     batchSize: 100,
     blockRange: {
