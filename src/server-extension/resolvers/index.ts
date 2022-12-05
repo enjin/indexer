26fe673ba09dab0de76aeaecd8af5c997c6def50
Leonardo Custodio <leonardo@custodio.me>
2022-12-05 17:19:15 -0300

    [PLA-472] Add verify message query (#107)
    Add verify message

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,3 +1,4 @@
 import { TokenSalesHistoryResolver } from './token-sales-history'
+import { VerifyMessageResolver } from './verify_message'
 
-export { TokenSalesHistoryResolver }
+export { TokenSalesHistoryResolver, VerifyMessageResolver }
