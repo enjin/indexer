25947c0f7797b92750985e96113db196dee1fbc5
Raman Shekhawat <ramanshekhawat75@gmail.com>
2023-02-28 14:31:15 +0530

    fix

--- src/common/tools.ts
+++ src/common/tools.ts
@@ -75,10 +75,13 @@ export function saturatingSumBigInt(
 }
 
 // eslint-disable-next-line no-control-regex
-const regex = /\/\/u0000/ // null unicode
+const regex = /\/\/u0000/ // null string unicode
+
+// eslint-disable-next-line no-control-regex
+const regex2 = /\u0000/ // null byte unicode
 
 export function safeString(s: string) {
-    if (regex.test(s)) {
+    if (regex.test(s) || regex2.test(s)) {
         return stringToHex(s)
     }
     return s
