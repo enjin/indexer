eb92a2a4d232fef3e50c248bf1eec46b29271c0b
Leonardo Custodio <leonardo@custodio.me>
2023-04-07 17:23:59 -0300

    Add Claims

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,5 +1,6 @@
 import { RefreshMetadataResolver } from './refresh_metadata'
 import { TokenSalesHistoryResolver } from './token_sales_history'
 import { VerifyMessageResolver } from './verify_message'
+import { NewClaimResolver } from './new_claim'
 
-export { TokenSalesHistoryResolver, VerifyMessageResolver, RefreshMetadataResolver }
+export { NewClaimResolver, TokenSalesHistoryResolver, VerifyMessageResolver, RefreshMetadataResolver }
