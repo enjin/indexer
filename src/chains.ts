a340a1d0010c77aba5390e263e0cb9b588f53851
Leonardo Custodio <leonardo@custodio.me>
2023-04-19 13:39:47 -0300

    Add matrix canary

--- src/chains.ts
+++ src/chains.ts
@@ -29,6 +29,21 @@ const chains = [
         genesisHash: '0x1cb2120b3afd6da2dca23d8b95e30fd7eabe4357c3d470bc02487a7d6fd00d1d',
         website: 'https://efinity.io/',
     },
+    {
+        name: 'matrix-canary',
+        displayName: 'matrix-canary',
+        prefix: 13400,
+        tokens: [
+            {
+                symbol: 'cENJ',
+                decimals: 18,
+            },
+        ],
+        paraId: 1000,
+        relay: 'enjin-chain',
+        genesisHash: '0x364d2ef585c308e4c3d38c410db2e0b4bb1ea5fa4e67a009ac9d352839fc2790',
+        website: 'https://efinity.io/',
+    },
 ] as const
 
 export default chains
