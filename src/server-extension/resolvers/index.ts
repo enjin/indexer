73fbcd86d24a350f6812b499d42b5a6b0ed582bc
Leonardo Custodio <leonardo@custodio.me>
2023-05-08 14:43:16 -0300

    [PLA-935] Remove newclaim resolver (#399)
    * remove new claim
    
    * remove new claim

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,6 +1,5 @@
 import { RefreshMetadataResolver } from './refresh_metadata'
 import { TokenSalesHistoryResolver } from './token_sales_history'
 import { VerifyMessageResolver } from './verify_message'
-import { NewClaimResolver } from './new_claim'
 
-export { NewClaimResolver, TokenSalesHistoryResolver, VerifyMessageResolver, RefreshMetadataResolver }
+export { TokenSalesHistoryResolver, VerifyMessageResolver, RefreshMetadataResolver }
