27cba44df51e048cba3fd2f6131df6fed2702ac9
Raman Shekhawat <ramanshekhawat75@gmail.com>
2023-07-11 16:49:25 +0530

    hot: Fix indexing (#490)
    hot: fix indexing

--- src/mappings/multiTokens/events/token_destroyed.ts
+++ src/mappings/multiTokens/events/token_destroyed.ts
@@ -72,15 +72,20 @@ export async function tokenDestroyed(
                 id: token.id,
             },
         })
+        token.bestListing = null
+        token.recentListing = null
+        await ctx.store.save(token)
 
         const listings = [...makeListings, ...takeListings]
-        await ctx.store.remove(listings)
-        await ctx.store.delete(TraitToken, { token: { id: token.id } })
         // TODO: We are removing all events that are related to this token.
         // We should only update the events that have relationship so it is null.
         // await ctx.store.delete(Event, { tokenId: token.id })
-        await ctx.store.delete(AccountTokenEvent, { token: { id: token.id } })
-        await ctx.store.remove(attributes)
+        await Promise.all([
+            await ctx.store.remove(listings),
+            await ctx.store.delete(TraitToken, { token: { id: token.id } }),
+            await ctx.store.delete(AccountTokenEvent, { token: { id: token.id } }),
+            await ctx.store.remove(attributes),
+        ])
     }
 
     await ctx.store.remove(token)
