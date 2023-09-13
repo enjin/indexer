0865675824b2255d75c3c647ea28e8d980715d74
Raman Shekhawat <ramanshekhawat75@gmail.com>
2023-09-13 22:11:39 +0530

    feat: handle rejected claims in indexer (#616)
    * add claimRejected
    
    * partial
    
    * add claimMinted event
    
    * fix

---
+++ src/mappings/claims/events/claimMinted.ts
@@ -0,0 +1,56 @@
+import { SubstrateBlock } from '@subsquid/substrate-processor'
+import { EventItem } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
+import { u8aToHex } from '@polkadot/util'
+import { UnknownVersionError } from '../../../common/errors'
+import { ClaimsClaimMintedEvent } from '../../../types/generated/events'
+import { AccountClaimType, ClaimRequest, ClaimDetails, ClaimAccount, Event as EventModel } from '../../../model'
+import { Event } from '../../../types/generated/support'
+import { CommonContext } from '../../types/contexts'
+import { getTotalUnclaimedAmount } from '../common'
+
+function getEventData(ctx: CommonContext, event: Event) {
+    const data = new ClaimsClaimMintedEvent(ctx, event)
+
+    if (data.isMatrixEnjinV603) {
+        return data.asMatrixEnjinV603
+    }
+
+    throw new UnknownVersionError(data.constructor.name)
+}
+
+export async function claimMinted(
+    ctx: CommonContext,
+    block: SubstrateBlock,
+    item: EventItem<'Claims.ClaimMinted', { event: { args: true; extrinsic: true } }>
+): Promise<EventModel | undefined> {
+    const eventData = getEventData(ctx, item.event)
+
+    if (!eventData) return undefined
+
+    const account = new ClaimAccount({
+        type: AccountClaimType.EVM,
+        account: u8aToHex(eventData.who),
+    })
+
+    const claim = new ClaimRequest({
+        id: `${u8aToHex(eventData.who)}-${block.height}-minted`,
+        account,
+        amountClaimable: eventData.amount,
+        amountBurned: 0n,
+        hash: null,
+        isEfiToken: false,
+        extrinsicIndex: block.height,
+        isClaimed: false,
+        createdBlock: block.height,
+        createdAt: new Date(block.timestamp),
+    })
+
+    const claimDetails = new ClaimDetails({
+        id: '0',
+        totalUnclaimedAmount: await getTotalUnclaimedAmount(ctx, block),
+    })
+
+    await Promise.all([ctx.store.insert(ClaimRequest, claim), ctx.store.save(claimDetails)])
+
+    return undefined
+}
--- src/processor.ts
+++ src/processor.ts
@@ -85,6 +85,8 @@ const processor = new SubstrateBatchProcessor()
     .addEvent('Claims.ClaimRequested', eventOptions)
     .addEvent('Claims.DelayTimeForClaimSet', eventOptions)
     .addEvent('Claims.ExchangeRateSet', eventOptions)
+    .addEvent('Claims.ClaimRejected', eventOptions)
+    .addEvent('Claims.ClaimMinted', eventOptions)
     // eslint-disable-next-line sonarjs/no-duplicate-string
     .addEvent('Balances.Withdraw', eventOptions)
     .addEvent('Balances.BalanceSet', eventOptions)
@@ -162,6 +164,10 @@ async function handleEvents(
             return map.balances.processor.save(ctx, block, item.event)
         case 'Claims.ClaimRequested':
             return map.claims.events.claimRequested(ctx, block, item)
+        case 'Claims.ClaimRejected':
+            return map.claims.events.claimRejected(ctx, block, item)
+        case 'Claims.ClaimMinted':
+            return map.claims.events.claimMinted(ctx, block, item)
         case 'Claims.DelayTimeForClaimSet':
             return map.claims.events.delayTimeForClaimSet(ctx, block, item)
         case 'Claims.Claimed':
