12aa6e0c801b1aaee531b2070f9df56855186420
Raman Shekhawat <ramanshekhawat75@gmail.com>
2023-09-22 22:55:49 +0530

    rule set Removed

---
+++ src/mappings/fuelTanks/events/ruleSetRemoved.ts
@@ -0,0 +1,34 @@
+import { SubstrateBlock } from '@subsquid/substrate-processor'
+import { EventItem } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
+import { u8aToHex } from '@polkadot/util'
+import { UnknownVersionError } from '../../../common/errors'
+import { FuelTanksRuleSetRemovedEvent } from '../../../types/generated/events'
+import { Event as EventModel, FuelTankRuleSet } from '../../../model'
+import { Event } from '../../../types/generated/support'
+import { CommonContext } from '../../types/contexts'
+
+function getEventData(ctx: CommonContext, event: Event) {
+    const data = new FuelTanksRuleSetRemovedEvent(ctx, event)
+
+    if (data.isMatrixEnjinV603) {
+        return data.asMatrixEnjinV603
+    }
+
+    throw new UnknownVersionError(data.constructor.name)
+}
+
+export async function ruleSetRemoved(
+    ctx: CommonContext,
+    block: SubstrateBlock,
+    item: EventItem<'FuelTanks.RuleSetRemoved', { event: { args: true; extrinsic: true } }>
+): Promise<EventModel | undefined> {
+    const eventData = getEventData(ctx, item.event)
+
+    if (!eventData) return undefined
+
+    const ruleId = `${u8aToHex(eventData.tankId)}-${eventData.ruleSetId}`
+
+    ctx.store.delete(FuelTankRuleSet, { id: ruleId })
+
+    return undefined
+}
