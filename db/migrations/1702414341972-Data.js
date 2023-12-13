5f596dea6b2c880b538a50ba3129b296649e2b6e
Leonardo Custodio <leonardo@enjin.io>
2023-12-13 18:46:07 -0300

    change

--- db/migrations/1702414341972-Data.js
+++ db/migrations/1702414341972-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1702414341971 {
-    name = 'Data1702414341971'
+module.exports = class Data1702414341972 {
+    name = 'Data1702414341972'
 
     async up(db) {
         await db.query(`CREATE INDEX "IDX_cab3c454b0419a03584a3990ce" ON "token" ("token_id") `)
