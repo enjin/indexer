f990d4333ab6a8a95bfb9008ee2797e0b8f22c94
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-01-12 13:48:45 +0530

    increase redirects and detect mime from file extension

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ "$role" = "processor" ]; then
     npm run db:migrate
     npm run processor:debug
 elif [ "$role" = "graphql" ]; then
-    npx squid-graphql-server --subscriptions --dumb-cache in-memory --dumb-cache-ttl 12000 --dumb-cache-size 512 --dumb-cache-max-age 12000 --max-root-fields 10 --sql-statement-timeout 5000
+    npx squid-graphql-server --subscriptions --dumb-cache in-memory --dumb-cache-ttl 12000 --dumb-cache-size 512 --dumb-cache-max-age 12000 --max-root-fields 10 --sql-statement-timeout 30000
 elif [ "$role" = "worker" ]; then
     npm run worker:start
 else
