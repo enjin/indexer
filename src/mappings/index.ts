1d45eda8ab65472ca20384291508ad77c6cb0b10
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-01-25 02:05:05 +0530

    fix

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -4,5 +4,6 @@ import marketplace from './marketplace'
 import claims from './claims'
 import xcm from './xcm'
 import fuelTanks from './fuelTanks'
+import identity from './identity'
 
-export { multiTokens, balances, marketplace, claims, xcm, fuelTanks }
+export { multiTokens, balances, marketplace, claims, xcm, fuelTanks, identity }
