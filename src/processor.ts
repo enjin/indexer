e51f40fe71466cd2750eee519c1836c18fe2a2e6
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-02-08 20:04:26 +0530

    fix: [WAPI-1318] Add seller as participants when the `fill_listing` is called via fuelTank (#874)
    * add listing sellers as participants when the fill_listing is called via fuelTanks
    
    * Fix
    
    * Fix
    
    ---------
    
    Co-authored-by: Leonardo Custodio <leonardo@custodio.me>

--- src/processor.ts
+++ src/processor.ts
@@ -475,8 +475,17 @@ processor.run(
                         }
 
                         // Hotfix for adding listing seller to participant
-                        if (call.name === 'Marketplace.fill_listing' || call.name === 'Marketplace.finalize_auction') {
-                            const listingId = call.args.listingId.toString()
+                        if (
+                            call.name === 'Marketplace.fill_listing' ||
+                            call.name === 'Marketplace.finalize_auction' ||
+                            (fuelTank &&
+                                call.args.call?.__kind === 'Marketplace' &&
+                                call.args.call?.value?.__kind === 'fill_listing') ||
+                            (fuelTank &&
+                                call.args.call?.__kind === 'Marketplace' &&
+                                call.args.call?.value?.__kind === 'finalize_auction')
+                        ) {
+                            const listingId = call.args.call?.value?.listingId ?? call.args.listingId
                             // eslint-disable-next-line no-await-in-loop
                             const listing = await ctx.store.findOne(Listing, {
                                 where: { id: hexStripPrefix(listingId) },
