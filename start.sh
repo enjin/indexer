47b0c543280cb647a05de9013c4711469f2b1ccb
Leonardo Custodio <leonardo@custodio.me>
2024-02-13 16:46:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ "$role" = "processor" ]; then
     npm run db:migrate
     npm run processor:debug
 elif [ "$role" = "graphql" ]; then
-    npx squid-graphql-server --subscriptions --dumb-cache redis --dumb-cache-max-age 3000 --max-root-fields 10 --sql-statement-timeout 5000
+    npx squid-graphql-server --subscriptions --dumb-cache redis --dumb-cache-max-age 3000 --max-root-fields 10 --sql-statement-timeout 15000
 elif [ "$role" = "worker" ]; then
     npm run worker:start
 else
