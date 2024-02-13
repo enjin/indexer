6578f538b12dc9915419fc19d52ca829be29e3d3
Leonardo Custodio <leonardo@custodio.me>
2024-02-13 14:46:09 -0300

    [PLA-1606] Fixes issue with collection destroyed crashing (#889)
    * Fixes
    
    * more changes
    
    * more changes
    
    * fixes
    
    * lint

--- src/mappings/multiTokens/events/collection_destroyed.ts
+++ src/mappings/multiTokens/events/collection_destroyed.ts
@@ -4,12 +4,17 @@ import { u8aToHex } from '@polkadot/util'
 import { UnknownVersionError } from '../../../common/errors'
 import { MultiTokensCollectionDestroyedEvent } from '../../../types/generated/events'
 import {
+    AccountTokenEvent,
     Attribute,
     Collection,
+    CollectionAccount,
     Event as EventModel,
     Extrinsic,
+    Listing,
     MultiTokensCollectionDestroyed,
     RoyaltyCurrency,
+    Token,
+    TokenAccount,
     Trait,
 } from '../../../model'
 import { CommonContext } from '../../types/contexts'
@@ -57,10 +62,21 @@ export async function collectionDestroyed(
 
     const collectionId = data.collectionId.toString()
 
+    const accountTokenEvent = await ctx.store.find(AccountTokenEvent, { where: { token: { collection: { id: collectionId } } } })
+    const listingMake = await ctx.store.find(Listing, { where: { makeAssetId: { collection: { id: collectionId } } } })
+    const listingTake = await ctx.store.find(Listing, { where: { takeAssetId: { collection: { id: collectionId } } } })
+
+    await ctx.store.remove(AccountTokenEvent, accountTokenEvent)
+    await ctx.store.remove(Listing, listingMake)
+    await ctx.store.remove(Listing, listingTake)
+
     await Promise.all([
         ctx.store.delete(Trait, { collection: { id: collectionId } }),
         ctx.store.delete(RoyaltyCurrency, { collection: { id: collectionId } }),
         ctx.store.delete(Attribute, { collection: { id: collectionId } }),
+        ctx.store.delete(TokenAccount, { collection: { id: collectionId } }),
+        ctx.store.delete(Token, { collection: { id: collectionId } }),
+        ctx.store.delete(CollectionAccount, { collection: { id: collectionId } }),
     ])
 
     await ctx.store.delete(Collection, { id: collectionId })
