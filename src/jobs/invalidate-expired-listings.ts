ae5c659c9b0d17085856b373d51c1cf38d264ce8
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-03-01 01:43:08 +0530

    run invaldiate every minute

--- src/jobs/invalidate-expired-listings.ts
+++ src/jobs/invalidate-expired-listings.ts
@@ -2,7 +2,7 @@ import Queue from 'bull'
 import { redisConfig } from './common'
 
 export const invalidateExpiredListings = new Queue('InvalidateListings', {
-    defaultJobOptions: { repeat: { cron: '*/5 * * * *' }, attempts: 5, removeOnComplete: 300 },
+    defaultJobOptions: { repeat: { cron: '* * * * *' }, attempts: 5, removeOnComplete: 300 },
     redis: redisConfig,
     settings: {
         maxStalledCount: 0,
