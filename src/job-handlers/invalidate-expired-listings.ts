bfa50810d50082a32dc6917239e822a4404a2423
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-03-04 19:08:20 +0530

    wrap around tx

--- src/job-handlers/invalidate-expired-listings.ts
+++ src/job-handlers/invalidate-expired-listings.ts
@@ -9,24 +9,23 @@ export default async (job: Queue.Job, done: Queue.DoneCallback) => {
         })
     }
 
-    const em = connection.manager
+    connection.manager.transaction('READ COMMITTED', async (em) => {
+        const status: { height: number }[] = await em.query(`SELECT height FROM squid_processor.status WHERE id = 0`)
+        if (status.length === 0) {
+            done(null, null)
+        }
 
-    const status: { height: number }[] = await em.query(`SELECT height FROM squid_processor.status WHERE id = 0`)
-    if (status.length === 0) {
-        done(null, null)
-    }
-    const { height } = status[0]
-
-    const q = await em
-        .getRepository(Listing)
-        .createQueryBuilder('listing')
-        .update()
-        .set({ isActive: false })
-        .where('listing.is_active = true')
-        .andWhere('listing.type = :type', { type: ListingType.Auction })
-        .andWhere("(listing.data->>'endHeight')::int < :height ", { height })
-        .returning('id')
-        .execute()
-
-    done(null, { at: height, affected: q.affected, raw: q.raw })
+        const { height } = status[0]
+        const q = await em
+            .getRepository(Listing)
+            .createQueryBuilder('listing')
+            .update()
+            .set({ isActive: false })
+            .where('listing.is_active = true')
+            .andWhere('listing.type = :type', { type: ListingType.Auction })
+            .andWhere("(listing.data->>'endHeight')::int < :height ", { height })
+            .returning('id')
+            .execute()
+        done(null, { at: height, affected: q.affected, raw: q.raw })
+    })
 }
