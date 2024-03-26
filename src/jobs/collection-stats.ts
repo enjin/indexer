5649deeb20c3f8d22b779efb9e4fdc3feff58c30
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-03-26 15:14:49 +0530

    tmp change

--- src/jobs/collection-stats.ts
+++ src/jobs/collection-stats.ts
@@ -7,7 +7,7 @@ import { computeTraits } from './compute-traits'
 export type JobData = { collectionId: string }
 
 export const collectionStatsQueue = new Queue<JobData>('collectionStatsQueue', {
-    defaultJobOptions: { delay: 1000, attempts: 2, removeOnComplete: true },
+    defaultJobOptions: { delay: 1000, attempts: 2, removeOnComplete: 10 },
     redis: redisConfig,
     settings: {
         maxStalledCount: 3,
