dcd4e2e0f5149ebcc31cc072d026ccc5556cf766
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-05-10 17:49:36 +0530

    increase traits job delay by 10 seconds

--- src/jobs/compute-traits.ts
+++ src/jobs/compute-traits.ts
@@ -4,7 +4,7 @@ import { redisConfig } from './common'
 export type JobData = { collectionId: string }
 
 export const traitsQueue = new Queue<JobData>('traitsQueue', {
-    defaultJobOptions: { delay: 3000, attempts: 2, removeOnComplete: true },
+    defaultJobOptions: { delay: 10_000, attempts: 2, removeOnComplete: true },
     redis: redisConfig,
     settings: {
         maxStalledCount: 3,
