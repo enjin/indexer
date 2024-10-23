9204d92b9618b13aa10e836b5b3aa54c7aced75f
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-10-23 06:12:12 +0530

    hard code lastBlockHeight

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { ProcessorConfig } from './types/custom/processorConfig'
 const config: ProcessorConfig = {
     chainName: process.env.CHAIN_NAME || 'enjin-matrixchain',
     prefix: process.env.CHAIN_PREFIX ? parseInt(process.env.CHAIN_PREFIX, 10) : 1110,
-    lastBlockHeight: process.env.LAST_BLOCK_HEIGHT ? parseInt(process.env.LAST_BLOCK_HEIGHT, 10) : 0,
+    lastBlockHeight: 2911918,
     genesisHash: process.env.GENESIS_HASH || '0x3af4ff48ec76d2efc8476730f423ac07e25ad48f5f4c9dc39c778b164d808615',
     dataSource: {
         archive: process.env.ARCHIVE_ENDPOINT || null,
