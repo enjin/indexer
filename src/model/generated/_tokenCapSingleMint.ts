3af1c498bda9c1f55ba6d4adbba59a31b0f02108
Raman Shekhawat <ramanshekhawat75@gmail.com>
2025-01-14 18:08:39 +0530

    feat: Add supply to collapsing cap (#1495)
    * fix
    
    * add supply to CollapsingSupply

--- src/model/generated/_tokenCapSingleMint.ts
+++ src/model/generated/_tokenCapSingleMint.ts
@@ -5,11 +5,13 @@ import {CapType} from "./_capType"
 export class TokenCapSingleMint {
     public readonly isTypeOf = 'TokenCapSingleMint'
     private _type!: CapType
+    private _supply!: bigint
 
     constructor(props?: Partial<Omit<TokenCapSingleMint, 'toJSON'>>, json?: any) {
         Object.assign(this, props)
         if (json != null) {
             this._type = marshal.enumFromJson(json.type, CapType)
+            this._supply = marshal.bigint.fromJSON(json.supply)
         }
     }
 
@@ -22,10 +24,20 @@ export class TokenCapSingleMint {
         this._type = value
     }
 
+    get supply(): bigint {
+        assert(this._supply != null, 'uninitialized access')
+        return this._supply
+    }
+
+    set supply(value: bigint) {
+        this._supply = value
+    }
+
     toJSON(): object {
         return {
             isTypeOf: this.isTypeOf,
             type: this.type,
+            supply: marshal.bigint.toJSON(this.supply),
         }
     }
 }
