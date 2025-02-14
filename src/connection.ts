3e3d3c1a944a3ae4a16ec6b79ba6fc98c3045e79
Leonardo Custodio <leonardo@custodio.me>
2025-02-14 12:07:44 -0300

    changes

--- src/connection.ts
+++ src/connection.ts
@@ -1,8 +1,15 @@
 import { DataSource } from 'typeorm'
 import { createOrmConfig } from '@subsquid/typeorm-config'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 
 const cfg: any = createOrmConfig()
-const con = new DataSource({ ...cfg, poolSize: 200, pool: { max: 100 } })
+const con = new DataSource({
+    ...cfg,
+    maxQueryExecutionTime: 5000,
+    poolSize: 200,
+    logging: ['error'],
+    logger: 'advanced-console',
+} as PostgresConnectionOptions)
 
 export default con
 
