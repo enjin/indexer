b5609ebe7e987a53dbeba67c4503328fb4786765
Leonardo Custodio <leonardo@custodio.me>
2025-02-19 11:27:24 -0300

    Add new data

--- src/model/generated/_fixedPriceData.ts
+++ src/model/generated/_fixedPriceData.ts
@@ -5,11 +5,13 @@ import {ListingType} from "./_listingType"
 export class FixedPriceData {
     public readonly isTypeOf = 'FixedPriceData'
     private _listingType!: ListingType
+    private _notUsed!: number | undefined | null
 
     constructor(props?: Partial<Omit<FixedPriceData, 'toJSON'>>, json?: any) {
         Object.assign(this, props)
         if (json != null) {
             this._listingType = marshal.enumFromJson(json.listingType, ListingType)
+            this._notUsed = json.notUsed == null ? undefined : marshal.int.fromJSON(json.notUsed)
         }
     }
 
@@ -22,10 +24,19 @@ export class FixedPriceData {
         this._listingType = value
     }
 
+    get notUsed(): number | undefined | null {
+        return this._notUsed
+    }
+
+    set notUsed(value: number | undefined | null) {
+        this._notUsed = value
+    }
+
     toJSON(): object {
         return {
             isTypeOf: this.isTypeOf,
             listingType: this.listingType,
+            notUsed: this.notUsed,
         }
     }
 }
