12d7257ec7b2b5e1e4a89b96d7998e6c400196c3
Leonardo Custodio <leonardo@custodio.me>
2025-02-21 16:18:53 -0300

    changes

--- src/worker/queue/jobs/delete-traits/delete-traits.processor.ts
+++ src/worker/queue/jobs/delete-traits/delete-traits.processor.ts
@@ -1,7 +1,11 @@
 import { Job } from 'bullmq'
 import { ProcessorDef } from '../processor.def'
+import { connectionManager } from '../../../../contexts'
 
 export default class DeleteTraitsProcessor implements ProcessorDef {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async handle(job: Job): Promise<void> {}
+    async handle(job: Job): Promise<void> {
+        const con = connectionManager()
+        await con.query(`DELETE FROM trait_token WHERE token_id = $1`, [job.data.id])
+        await con.query(`DELETE FROM token_rarity WHERE token_id = $1`, [job.data.id])
+    }
 }
