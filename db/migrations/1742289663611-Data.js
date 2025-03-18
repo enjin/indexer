2d1c52d0bdd1de8b6bc76faa17c56af03ebe420b
Leonardo Custodio <leonardo@custodio.me>
2025-03-18 06:47:45 -0300

    changes

--- db/migrations/1742289663611-Data.js
+++ db/migrations/1742289663611-Data.js
@@ -8,7 +8,9 @@ module.exports = class Data1742289663611 {
         await db.query(`CREATE INDEX "IDX_08fcb547f04afff252aae6ed4d" ON "whitelisted_account" ("account_id") `)
         await db.query(`CREATE INDEX "IDX_2e16c26d1dbedada68a1784d7f" ON "whitelisted_account" ("listing_id") `)
         await db.query(`ALTER TABLE "listing" ADD "start_block" integer`)
-        await db.query(`ALTER TABLE "listing" ADD "creation_block" integer NOT NULL DEFAULT "height"`)
+        await db.query(`ALTER TABLE "listing" ADD "creation_block" integer`)
+        await db.query(`UPDATE "listing" SET "creation_block" = "height"`)
+        await db.query(`ALTER TABLE "listing" ALTER COLUMN "creation_block" SET NOT NULL`)
         await db.query(`ALTER TABLE "listing" ADD "uses_whitelist" boolean NOT NULL DEFAULT false`)
         await db.query(
             `ALTER TABLE "whitelisted_account" ADD CONSTRAINT "FK_08fcb547f04afff252aae6ed4d9" FOREIGN KEY ("account_id") REFERENCES "account"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
