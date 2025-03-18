169134b4d2460e13a0572afca75d4db9e5dfa977
Leonardo Custodio <leonardo@custodio.me>
2025-03-18 11:31:34 -0300

    Test

--- src/model/generated/_marketPolicy.ts
+++ src/model/generated/_marketPolicy.ts
@@ -4,13 +4,13 @@ import {Royalty} from "./_royalty"
 
 export class MarketPolicy {
     private _royalty!: Royalty | undefined | null
-    private _beneficiaries!: (Royalty | undefined | null)[]
+    private _beneficiaries!: (Royalty)[]
 
     constructor(props?: Partial<Omit<MarketPolicy, 'toJSON'>>, json?: any) {
         Object.assign(this, props)
         if (json != null) {
             this._royalty = json.royalty == null ? undefined : new Royalty(undefined, json.royalty)
-            this._beneficiaries = marshal.fromList(json.beneficiaries, val => val == null ? undefined : new Royalty(undefined, val))
+            this._beneficiaries = marshal.fromList(json.beneficiaries, val => new Royalty(undefined, marshal.nonNull(val)))
         }
     }
 
@@ -22,19 +22,19 @@ export class MarketPolicy {
         this._royalty = value
     }
 
-    get beneficiaries(): (Royalty | undefined | null)[] {
+    get beneficiaries(): (Royalty)[] {
         assert(this._beneficiaries != null, 'uninitialized access')
         return this._beneficiaries
     }
 
-    set beneficiaries(value: (Royalty | undefined | null)[]) {
+    set beneficiaries(value: (Royalty)[]) {
         this._beneficiaries = value
     }
 
     toJSON(): object {
         return {
             royalty: this.royalty == null ? undefined : this.royalty.toJSON(),
-            beneficiaries: this.beneficiaries.map((val: any) => val == null ? undefined : val.toJSON()),
+            beneficiaries: this.beneficiaries.map((val: any) => val.toJSON()),
         }
     }
 }
