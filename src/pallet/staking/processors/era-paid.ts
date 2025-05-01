8eeddaa4bee05fa4143f16a0eb850b427ef63300
Leonardo Custodio <leonardo@custodio.me>
2025-05-01 11:47:49 -0300

    changes

--- src/pallet/staking/processors/era-paid.ts
+++ src/pallet/staking/processors/era-paid.ts
@@ -27,6 +27,7 @@ export async function eraPaid(ctx: CommonContext, block: Block, item: EventItem)
     })
 
     await ctx.store.save(era)
+    //  await computeValidators()
 
     return new EventModel({
         id: item.id,
