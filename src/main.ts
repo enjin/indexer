5ad231ec6492319f512ca64cc6be39cc0730920e
Leonardo Custodio <leonardo@custodio.me>
2025-05-09 09:21:04 -0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -211,10 +211,10 @@ async function bootstrap() {
                         }
                     }
 
-                    // if (block.header.height > config.lastBlockHeight) {
-                    //     p.balances.processors.addAccountsToSet(Array.from(signers))
-                    //     await p.balances.processors.saveAccounts(ctx as unknown as CommonContext, block.header)
-                    // }
+                    if (block.header.height > dataService.lastBlockNumber) {
+                        p.balances.processors.addAccountsToSet(Array.from(signers))
+                        await p.balances.processors.saveAccounts(ctx as unknown as CommonContext, block.header)
+                    }
 
                     for (const chunk of _.chunk(extrinsics, 1000)) {
                         void ctx.store.insert(chunk)
