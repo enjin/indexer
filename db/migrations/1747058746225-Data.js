2f87131f561f2c5097a1a7f4d4105870c2fc5511
Leonardo Custodio <leonardo@custodio.me>
2025-05-12 11:15:20 -0300

    changes

--- db/migrations/1747058746225-Data.js
+++ db/migrations/1747058746225-Data.js
@@ -3,19 +3,9 @@ module.exports = class Data1747058746225 {
 
     async up(db) {
         await db.query(`ALTER TABLE "account_token_event" ADD "meta" jsonb`)
-        await db.query(`ALTER TABLE "account_token_event" ALTER COLUMN "collection_id" DROP DEFAULT`)
-        await db.query(`DROP INDEX "public"."IDX_87fb00cfed5217b47de01f5856"`)
-        await db.query(`ALTER TABLE "account_token_event" DROP COLUMN "token_id"`)
-        await db.query(`ALTER TABLE "account_token_event" ADD "token_id" text NOT NULL`)
-        await db.query(`CREATE INDEX "IDX_87fb00cfed5217b47de01f5856" ON "account_token_event" ("token_id") `)
     }
 
     async down(db) {
         await db.query(`ALTER TABLE "account_token_event" DROP COLUMN "meta"`)
-        await db.query(`ALTER TABLE "account_token_event" ALTER COLUMN "collection_id" SET DEFAULT '0'`)
-        await db.query(`CREATE INDEX "IDX_87fb00cfed5217b47de01f5856" ON "account_token_event" ("token_id") `)
-        await db.query(`ALTER TABLE "account_token_event" ADD "token_id" character varying NOT NULL`)
-        await db.query(`ALTER TABLE "account_token_event" DROP COLUMN "token_id"`)
-        await db.query(`DROP INDEX "public"."IDX_87fb00cfed5217b47de01f5856"`)
     }
 }
