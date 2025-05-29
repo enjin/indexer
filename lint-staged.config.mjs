0d9c7de5dc6f3b01b14a5e242174626e2b90b7fc
Leonardo Custodio <leonardo@custodio.me>
2025-05-29 09:46:42 -0300

    changes

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,6 +2,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-    '!(*.ts)': 'prettier --write',
-    '*.ts': ['eslint --fix', 'prettier --write'],
+    '!(*.ts)': 'prettier . --write --config prettier.config.mjs',
+    '*.ts': ['eslint --fix', 'prettier . --write --config prettier.config.mjs'],
 }
