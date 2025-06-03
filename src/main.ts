f48547348d394ce4bf5fdce4cec9fff8c7ca1a33
Leonardo Custodio <leonardo@custodio.me>
2025-06-03 09:16:57 -0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -210,13 +210,13 @@ async function bootstrap() {
                     }
 
                     for (const chunk of _.chunk(extrinsics, 1000)) {
-                        await ctx.store.insert(chunk)
+                        void ctx.store.insert(chunk)
                     }
                     for (const chunk of _.chunk(eventsCollection, 1000)) {
-                        await ctx.store.insert(chunk)
+                        void ctx.store.insert(chunk)
                     }
                     for (const chunk of _.chunk(accountTokenEvents, 1000)) {
-                        await ctx.store.insert(chunk)
+                        void ctx.store.insert(chunk)
                     }
                 }
 
