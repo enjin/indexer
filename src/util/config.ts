aab09212245e6b5111b0b7f9109acad070075ff0
Leonardo Custodio <leonardo@custodio.me>
2025-06-11 13:38:06 -0300

    fix app env

--- src/util/config.ts
+++ src/util/config.ts
@@ -42,7 +42,7 @@ export interface Config {
 }
 
 const config: Config = {
-    environment: process.env.NODE_ENV || 'local',
+    environment: process.env.APP_ENV || 'local',
     chainName: process.env.CHAIN_NAME || 'enjin-matrixchain',
     prefix: process.env.CHAIN_PREFIX ? parseInt(process.env.CHAIN_PREFIX, 10) : 1110,
     genesisHash: process.env.GENESIS_HASH || '0x3af4ff48ec76d2efc8476730f423ac07e25ad48f5f4c9dc39c778b164d808615',
