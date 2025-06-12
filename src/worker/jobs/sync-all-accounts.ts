6f22e79a4fd435b319757af57b5cc9e3d6cab865
Zouheir Layine <zlayine@enjin.io>
2025-06-12 07:30:39 +0100

    fix address

--- src/worker/jobs/sync-all-accounts.ts
+++ src/worker/jobs/sync-all-accounts.ts
@@ -6,11 +6,11 @@ import { Job } from 'bullmq'
 export async function syncAllAccounts(job: Job) {
     const em = await connectionManager()
     const accounts = await em.find(Account, {
-        select: ['id'],
+        select: ['id', 'address'],
     })
 
     for (let i = 0; i < accounts.length; i += 50) {
-        QueueUtils.dispatchFetchAccounts(accounts.slice(i, i + 50).map((account) => account.id))
+        QueueUtils.dispatchFetchAccounts(accounts.slice(i, i + 50).map((account) => account.address))
     }
 
     await job.log(`Dispatched fetchAccountsDetail for ${accounts.length} accounts`)
