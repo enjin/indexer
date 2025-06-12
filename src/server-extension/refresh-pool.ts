a7036ffd0af6a27aaf45de30965eadd91a9989a2
Zouheir Layine <zlayine@enjin.io>
2025-06-12 09:36:53 +0100

    add refresh pool

---
+++ src/server-extension/refresh-pool.ts
@@ -0,0 +1,17 @@
+import { Arg, Query, Resolver } from 'type-graphql'
+import 'reflect-metadata'
+import { type EntityManager } from 'typeorm'
+import { NominationPool } from '../model'
+import { QueueUtils } from 'src/queue'
+
+@Resolver()
+export class RefreshPoolResolver {
+    constructor(private tx: () => Promise<EntityManager>) {}
+
+    @Query(() => Boolean)
+    async refreshPool(@Arg('id', () => String) id: string): Promise<boolean> {
+        QueueUtils.dispatchRefreshPool(id)
+
+        return true
+    }
+}
