732fcd23c153c169d564b3f92b873a69ae38df6f
Zouheir Layine <zlayine@enjin.io>
2025-06-26 18:48:31 +0100

    fix

--- src/worker/jobs/refresh-listings.ts
+++ src/worker/jobs/refresh-listings.ts
@@ -1,4 +1,4 @@
-import { Listing } from '../../model'
+import { AuctionData, Listing, ListingType } from '../../model'
 import { connectionManager } from '../../contexts'
 import { Job } from 'bullmq'
 import Rpc from '../../util/rpc'
@@ -8,20 +8,28 @@ export async function refreshListings(job: Job, ids: string[]) {
     const { api } = await Rpc.getInstance()
 
     for (const id of ids) {
-        const listingData = await api.query.marketplace.listings(id)
+        const listingData = await api.query.marketplace.listings(`0x${id}`)
         const listing = await em.findOneBy(Listing, { id })
+        let hasChanged = false
 
         if (!listing) {
             await job.log(`Listing ${id} not found`)
         }
 
-        const listingJson: any = listingData.toJSON()
+        if (listing?.data.isTypeOf === 'AuctionData') {
+            const listingJson: any = listingData.toJSON()
+            const endHeight = listingJson.data.auction.endBlock
+            listing.data = new AuctionData({
+                listingType: ListingType.Auction,
+                endHeight: endHeight,
+            })
+            hasChanged = true
+        }
 
-        const endHeight = listingJson.data.Auction.endBlock.toHuman()
-
-        await job.log(`End height: ${endHeight}`)
-
-        await job.log(`Refreshed ${id}`)
+        if (hasChanged) {
+            await em.save(listing)
+            await job.log(`Refreshed listing ${id}`)
+        }
     }
 
     await job.log(`Refreshed ${ids.length} listings`)
