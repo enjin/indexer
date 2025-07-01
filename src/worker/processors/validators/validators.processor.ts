3e40d9cfe225cc746d0ba24780e1f3715ed97836
Zouheir Layine <zlayine@enjin.io>
2025-07-01 11:56:34 +0100

    remove worker

--- src/worker/processors/validators/validators.processor.ts
+++ src/worker/processors/validators/validators.processor.ts
@@ -5,7 +5,6 @@ import { computeValidators } from '../../jobs/compute-validators'
 import { logDebug, logError } from '../../utils'
 import { syncValidators } from '../../jobs/sync-validators'
 import { syncChain } from '../../jobs/sync-chain'
-import { computeEraRewards } from '../../jobs/compute-era-rewards'
 
 export class ValidatorsProcessor implements ProcessorDef {
     async handle(job: Job): Promise<void> {
@@ -18,9 +17,6 @@ export class ValidatorsProcessor implements ProcessorDef {
                 break
             case JobsEnum.SYNC_CHAIN:
                 await syncChain(job, job.data.fromBlock, job.data.toBlock)
-                break
-            case JobsEnum.COMPUTE_ERA_REWARDS:
-                await computeEraRewards(job)
                 break
             default:
                 throw new Error(`${job.name} is not a valid job for this processor`)
