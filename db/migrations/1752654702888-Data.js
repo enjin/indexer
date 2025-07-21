ae62ad58170c4b7bf4d59d0680e44e835e08664f
Zouheir Layine <zlayine@enjin.io>
2025-07-21 10:08:41 +0100

    db hotfix

--- db/migrations/1752654702888-Data.js
+++ db/migrations/1752654702888-Data.js
@@ -2,16 +2,16 @@ module.exports = class Data1752654702888 {
     name = 'Data1752654702888'
 
     async up(db) {
-        await db.query(`ALTER TABLE "pool_member" ADD "accumulated_rewards" numeric`)
+        // await db.query(`ALTER TABLE "pool_member" ADD "accumulated_rewards" numeric`)
         await db.query(`ALTER TABLE "pool_member" ADD "is_stash" boolean`)
-        await db.query(`ALTER TABLE "collection" ADD "is_transfer_pending" boolean`)
-        await db.query(`ALTER TABLE "stake_exchange_offer" ADD "amount" numeric`)
+        // await db.query(`ALTER TABLE "collection" ADD "is_transfer_pending" boolean`)
+        // await db.query(`ALTER TABLE "stake_exchange_offer" ADD "amount" numeric`)
     }
 
     async down(db) {
-        await db.query(`ALTER TABLE "pool_member" DROP COLUMN "accumulated_rewards"`)
+        // await db.query(`ALTER TABLE "pool_member" DROP COLUMN "accumulated_rewards"`)
         await db.query(`ALTER TABLE "pool_member" DROP COLUMN "is_stash"`)
-        await db.query(`ALTER TABLE "collection" DROP COLUMN "is_transfer_pending"`)
-        await db.query(`ALTER TABLE "stake_exchange_offer" DROP COLUMN "amount"`)
+        // await db.query(`ALTER TABLE "collection" DROP COLUMN "is_transfer_pending"`)
+        // await db.query(`ALTER TABLE "stake_exchange_offer" DROP COLUMN "amount"`)
     }
 }
