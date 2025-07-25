c4a302493cf2df550c6a5eeede43a431ca843702
Zouheir Layine <zlayine@enjin.io>
2025-07-25 19:10:35 +0100

    log

--- src/worker/jobs/nomination-pools/compute-pool-rewards.ts
+++ src/worker/jobs/nomination-pools/compute-pool-rewards.ts
@@ -1,7 +1,7 @@
 import { NominationPool } from '~/model'
 import { dataHandlerContext } from '~/contexts'
 import { Job } from 'bullmq'
-import { computeEraApy, discardEra } from '~/pallet/nomination-pools/processors'
+import { discardEra } from '~/pallet/nomination-pools/processors'
 
 export async function computePoolRewards(_job: Job, id?: string): Promise<void> {
     const ctx = await dataHandlerContext()
@@ -16,7 +16,7 @@ export async function computePoolRewards(_job: Job, id?: string): Promise<void> 
         order: {
             eraRewards: {
                 era: {
-                    index: 'ASC',
+                    index: 'DESC',
                 },
             },
         },
@@ -34,9 +34,29 @@ export async function computePoolRewards(_job: Job, id?: string): Promise<void> 
 
     // count the last 15 eras
     const eraRewards = pool.eraRewards.slice(0, 15)
+    let sumOfRewards = 0
+    let previousCountedApy = 0
+    // discard the eras that have apy difference of more than 50% from the previous era
+    for (let i = 0; i < eraRewards.length; i++) {
+        const era = eraRewards[i]
 
-    const { sumOfRewards } = computeEraApy(eraRewards)
+        if (
+            era.apy > 0 &&
+            ((i !== 0 && !discardEra(era.apy, previousCountedApy)) ||
+                (i === 0 && !discardEra(era.apy, eraRewards[i + 1].apy)))
+        ) {
+            await _job.log(`Adding era ${era.id} to the sum of rewards ${era.apy}`)
+            previousCountedApy = era.apy
+            sumOfRewards += era.apy
+        } else {
+            await _job.log(
+                `Discarding era ${era.id} because of apy difference of more than 50% from the previous era, added ${previousCountedApy}`
+            )
+            sumOfRewards += previousCountedApy
+        }
+    }
 
+    await _job.log(`Sum of rewards: ${sumOfRewards}`)
     pool.apy = sumOfRewards / 15
 
     await ctx.store.save(pool)
