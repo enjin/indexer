aa60e95761c8282b72d71c365b2d072949c82f9a
Zouheir Layine <zlayine@enjin.io>
2025-08-11 10:01:37 +0100

    sync tokens as stream

--- src/worker/jobs/tokens/sync-tokens.ts
+++ src/worker/jobs/tokens/sync-tokens.ts
@@ -5,13 +5,15 @@ import { QueueUtils } from '~/queue'
 
 export async function syncTokens(job: Job) {
     const em = await connectionManager()
-    const tokens = await em.getRepository(Token).createQueryBuilder('token').select('token.id').getMany()
+    const stream = await em.getRepository(Token).createQueryBuilder('token').select('token.id').stream()
 
-    for (const token of tokens) {
+    stream.on('data', (data: any) => {
+        const token = data as Token
+
         // QueueUtils.dispatchComputeTokenSupply(token.id)
         // QueueUtils.dispatchComputeTokenBestListing(token.id)
         QueueUtils.dispatchComputeTokenInfusion(token.id)
-    }
+    })
 
-    await job.log(`Dispatched sync for ${tokens.length} tokens`)
+    await job.log(`Dispatched sync for tokens`)
 }
