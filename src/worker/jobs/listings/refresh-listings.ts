2a0a4bd36708a3ad21a4cfebfff1b1c9ae5ba59f
Zouheir Layine <zlayine@enjin.io>
2025-08-12 12:25:35 +0100

    fix refresh listings

--- src/worker/jobs/listings/refresh-listings.ts
+++ src/worker/jobs/listings/refresh-listings.ts
@@ -1,4 +1,4 @@
-import { AuctionData, AuctionState, Listing, ListingType } from '~/model'
+import { AuctionData, AuctionState, Listing, ListingType, OfferData, OfferState } from '~/model'
 import { connectionManager } from '~/contexts'
 import { Job } from 'bullmq'
 import Rpc from '~/util/rpc'
@@ -23,27 +23,39 @@ export async function refreshListings(job: Job, ids: string[]) {
     })
 
     for (const listing of listings) {
-        const listingData = await api.query.marketplace.listings(`0x${listing.id}`)
         let hasChanged = false
+        const listingData = await api.query.marketplace.listings(`0x${listing.id}`)
+        if (listing.type === ListingType.Offer) {
+            if (listing?.data.isTypeOf === 'OfferData') {
+                const listingJson: any = listingData.toJSON()
+                const expiration = listingJson.data.offer.expiration
+                listing.data = new OfferData({
+                    listingType: ListingType.Offer,
+                    expiration: expiration,
+                })
+                hasChanged = true
+            }
+        } else if (listing.type === ListingType.Auction) {
 
-        if (listing?.data.isTypeOf === 'AuctionData') {
-            const listingJson: any = listingData.toJSON()
-            const endHeight = listingJson.data.auction.endBlock
-            listing.data = new AuctionData({
-                listingType: ListingType.Auction,
-                endHeight: endHeight,
-            })
-            hasChanged = true
-        }
+            if (listing?.data.isTypeOf === 'AuctionData') {
+                const listingJson: any = listingData.toJSON()
+                const endHeight = listingJson.data.auction.endBlock
+                listing.data = new AuctionData({
+                    listingType: ListingType.Auction,
+                    endHeight: endHeight,
+                })
+                hasChanged = true
+            }
 
-        if (listing.state.isTypeOf === 'AuctionState') {
-            const highBid = listing.bids.length > 0 ? listing.bids[0].id : null
-            listing.state = new AuctionState({
-                listingType: ListingType.Auction,
-                highBid: highBid,
-                isExpired: listing.state.isExpired,
-            })
-            hasChanged = true
+            if (listing.state.isTypeOf === 'AuctionState') {
+                const highBid = listing.bids.length > 0 ? listing.bids[0].id : null
+                listing.state = new AuctionState({
+                    listingType: ListingType.Auction,
+                    highBid: highBid,
+                    isExpired: listing.state.isExpired,
+                })
+                hasChanged = true
+            }
         }
 
         if (hasChanged) {
