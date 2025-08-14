972b0dd4069c0d6511f6c1bc0ef58276a12a89f0
Pawel Wankiewicz <pawel@enjin.io>
2025-08-14 13:43:26 +0200

    NFTIO-2983 Adds rate to StakeExchaneOfferCompleted and StakeExchangeOfferCancelled events

---
+++ src/worker/jobs/nomination-pools/sync-stake-exchange-offers-events.ts
@@ -0,0 +1,47 @@
+import {Job} from "bullmq";
+import {connectionManager} from "~/contexts";
+import {
+    Event,
+    StakeExchangeBuyOrderCompleted, StakeExchangeOffer,
+    StakeExchangeOfferCancelled,
+    StakeExchangeOfferCompleted, StakeExchangeOfferCreated
+} from "~/model";
+import {EntityManager, In} from "typeorm";
+
+export async function syncStakeExchangeOffersEvents(job: Job): Promise<void> {
+    const em: EntityManager = await connectionManager()
+    const promises: Promise<any>[] = []
+
+    const events: Event[] = await em.find(Event, {
+        select: ['data', 'name'],
+        where: {
+            name: In([StakeExchangeOfferCancelled.name, StakeExchangeOfferCompleted.name, StakeExchangeOfferCreated.name, StakeExchangeBuyOrderCompleted.name])
+        }
+    })
+
+    for (const event of events) {
+        if (event.data && typeof event.data === "object") {
+            const data = event.data as Record<string, any>;
+
+            const offer: StakeExchangeOffer|null = await em.findOne(StakeExchangeOffer, {
+                select: ['rate'],
+                where: {
+                    offerId: data.offerId,
+                }
+            })
+
+            if (offer) {
+                data.rate = offer.rate
+
+                promises.push(em.save(Event, { ...offer, data }));
+            }
+        }
+    }
+
+    // Wait for all save operations to complete
+    await Promise.all(promises);
+
+    // Log success
+    await job.log(`Synced stake exchange offer rates`);
+
+}
