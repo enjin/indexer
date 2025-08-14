6921cc780dca0cea076c88395b7180617ce99061
Pawel Wankiewicz <pawel@enjin.io>
2025-08-14 13:47:27 +0200

    NFTIO-2983 Adds rate to StakeExchaneOfferCompleted and StakeExchangeOfferCancelled events

--- src/worker/jobs/nomination-pools/sync-stake-exchange-offers-events.ts
+++ src/worker/jobs/nomination-pools/sync-stake-exchange-offers-events.ts
@@ -1,12 +1,14 @@
-import {Job} from "bullmq";
-import {connectionManager} from "~/contexts";
+import { Job } from 'bullmq'
+import { connectionManager } from '~/contexts'
 import {
     Event,
-    StakeExchangeBuyOrderCompleted, StakeExchangeOffer,
+    StakeExchangeBuyOrderCompleted,
+    StakeExchangeOffer,
     StakeExchangeOfferCancelled,
-    StakeExchangeOfferCompleted, StakeExchangeOfferCreated
-} from "~/model";
-import {EntityManager, In} from "typeorm";
+    StakeExchangeOfferCompleted,
+    StakeExchangeOfferCreated,
+} from '~/model'
+import { EntityManager, In } from 'typeorm'
 
 export async function syncStakeExchangeOffersEvents(job: Job): Promise<void> {
     const em: EntityManager = await connectionManager()
@@ -15,33 +17,37 @@ export async function syncStakeExchangeOffersEvents(job: Job): Promise<void> {
     const events: Event[] = await em.find(Event, {
         select: ['data', 'name'],
         where: {
-            name: In([StakeExchangeOfferCancelled.name, StakeExchangeOfferCompleted.name, StakeExchangeOfferCreated.name, StakeExchangeBuyOrderCompleted.name])
-        }
+            name: In([
+                StakeExchangeOfferCancelled.name,
+                StakeExchangeOfferCompleted.name,
+                StakeExchangeOfferCreated.name,
+                StakeExchangeBuyOrderCompleted.name,
+            ]),
+        },
     })
 
     for (const event of events) {
-        if (event.data && typeof event.data === "object") {
-            const data = event.data as Record<string, any>;
+        if (event.data && typeof event.data === 'object') {
+            const data = event.data as Record<string, any>
 
-            const offer: StakeExchangeOffer|null = await em.findOne(StakeExchangeOffer, {
+            const offer: StakeExchangeOffer | null = await em.findOne(StakeExchangeOffer, {
                 select: ['rate'],
                 where: {
                     offerId: data.offerId,
-                }
+                },
             })
 
             if (offer) {
                 data.rate = offer.rate
 
-                promises.push(em.save(Event, { ...offer, data }));
+                promises.push(em.save(Event, { ...offer, data }))
             }
         }
     }
 
     // Wait for all save operations to complete
-    await Promise.all(promises);
+    await Promise.all(promises)
 
     // Log success
-    await job.log(`Synced stake exchange offer rates`);
-
+    await job.log(`Synced stake exchange offer rates`)
 }
