0199f4e25732277c6b96c36289a26372baaf0a95
Zouheir Layine <zlayine@enjin.io>
2025-09-16 11:38:49 +0100

    [PLA-2292] revert necessary refresh queries (#1684)
    revert necessary refresh queries

---
+++ src/server-extension/refresh-balances.ts
@@ -0,0 +1,31 @@
+import { Args, ArgsType, Field, Query, Resolver } from 'type-graphql'
+import { QueueUtils } from '~/queue'
+import { decodeAddress } from '~/util/tools'
+
+@ArgsType()
+export class RefreshBalancesArgs {
+    @Field(() => [String])
+    ids!: string[]
+}
+
+@Resolver()
+export class RefreshBalancesResolver {
+    @Query(() => Boolean, { nullable: false })
+    async refreshBalances(@Args() args: RefreshBalancesArgs): Promise<boolean> {
+        if (args.ids.length > 100) {
+            throw new Error('Too many accounts to refresh, limit is 100')
+        }
+
+        const publicKeys = args.ids.map((id) => {
+            if (!id.startsWith('0x')) {
+                return decodeAddress(id)
+            }
+
+            return id
+        })
+
+        QueueUtils.dispatchFetchBalances(publicKeys)
+
+        return true
+    }
+}
