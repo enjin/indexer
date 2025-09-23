81205f0730005c736c66234daa6f5ad086186449
Zouheir Layine <zlayine@enjin.io>
2025-09-23 12:20:52 +0100

    [NFTIO-3412] fix migration (#1688)
    fix migration

--- db/migrations/1758620036322-Data.js
+++ db/migrations/1758620036322-Data.js
@@ -2,7 +2,7 @@ module.exports = class Data1758620036322 {
     name = 'Data1758620036322'
 
     async up(db) {
-        await db.query(`ALTER TABLE "token_group" ADD "created_at" TIMESTAMP WITH TIME ZONE NOT NULL`)
+        await db.query(`ALTER TABLE "token_group" ADD "created_at" TIMESTAMP WITH TIME ZONE NULL`)
     }
 
     async down(db) {
