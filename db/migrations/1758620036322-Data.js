dbddc13cbced8e9778251853d4e18fd3d2e10eb6
Zouheir Layine <zlayine@enjin.io>
2025-09-23 10:44:36 +0100

    [NFTIO-3412] add token group migration (#1687)
    add token group migration

---
+++ db/migrations/1758620036322-Data.js
@@ -0,0 +1,11 @@
+module.exports = class Data1758620036322 {
+    name = 'Data1758620036322'
+
+    async up(db) {
+        await db.query(`ALTER TABLE "token_group" ADD "created_at" TIMESTAMP WITH TIME ZONE NOT NULL`)
+    }
+
+    async down(db) {
+        await db.query(`ALTER TABLE "token_group" DROP COLUMN "created_at"`)
+    }
+}
