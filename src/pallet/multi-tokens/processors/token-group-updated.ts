d8302c9c5cd7fe3b8fef86a7504b0a00604d1734
Zouheir Layine <zlayine@enjin.io>
2025-09-26 14:24:16 +0100

    [NFTIO-3421] fix token group ordering (#1691)
    * fix token group ordering
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add index
    
    * fix
    
    * add to relation
    
    * fix

--- src/pallet/multi-tokens/processors/token-group-updated.ts
+++ src/pallet/multi-tokens/processors/token-group-updated.ts
@@ -13,8 +13,8 @@ export async function tokenGroupUpdated(
 
     const tokenGroupIds = data.tokenGroups.map((tokenGroupId) => tokenGroupId.toString())
 
-    const [tokenGroup, token] = await Promise.all([
-        ctx.store.findOneOrFail(TokenGroup, {
+    const [tokenGroups, token] = await Promise.all([
+        ctx.store.find(TokenGroup, {
             where: {
                 id: In(tokenGroupIds),
             },
@@ -23,14 +23,23 @@ export async function tokenGroupUpdated(
             where: {
                 id: `${data.collectionId.toString()}-${data.tokenId.toString()}`,
             },
+            relations: {
+                tokenGroupTokens: true,
+            },
         }),
     ])
 
-    const tokenGroupTokens = tokenGroupIds.map((tokenGroupId) => {
+    const promises = []
+    for (const tokenGroupToken of token.tokenGroupTokens) {
+        promises.push(ctx.store.remove(tokenGroupToken))
+    }
+    await Promise.all(promises)
+
+    const tokenGroupTokens = tokenGroupIds.map((tokenGroupId, index) => {
         return new TokenGroupToken({
-            id: `${data.tokenId.toString()}-${tokenGroupId}`,
+            id: `${data.tokenId.toString()}-${index}-${tokenGroupId}`,
             token,
-            tokenGroup,
+            tokenGroup: tokenGroups.find((tokenGroup) => tokenGroup.id === tokenGroupId),
         })
     })
 
