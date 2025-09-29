9246803026dc2cf61d6d871aeb48c0c4d1b9be2d
Zouheir Layine <zlayine@enjin.io>
2025-09-29 13:26:07 +0100

    fix wrong ids

--- src/worker/jobs/tokens/sync-token-group-positions.ts
+++ src/worker/jobs/tokens/sync-token-group-positions.ts
@@ -5,33 +5,37 @@ import { TokenGroup, TokenGroupToken } from '~/model'
 export async function syncTokenGroupPositions(job: Job) {
     const ctx = await dataHandlerContext()
 
-    const tokenGroups = await ctx.store.find(TokenGroup, {
+    const tokenGroupTokens = await ctx.store.find(TokenGroupToken, {
         relations: {
-            tokenGroupTokens: {
-                token: true,
-            },
+            token: true,
+            tokenGroup: true,
         },
     })
 
-    for (const tokenGroup of tokenGroups) {
-        for (const [index, tokenGroupToken] of tokenGroup.tokenGroupTokens.entries()) {
-            if (tokenGroupToken.id !== `${tokenGroupToken.token.tokenId}-${tokenGroup.id}`) {
-                const token = tokenGroupToken.token
-                await ctx.store.remove(tokenGroupToken)
-
-                const newTokenGroupToken = new TokenGroupToken({
-                    id: `${token.tokenId}-${tokenGroup.id}`,
-                    token,
-                    tokenGroup,
-                    position: index,
-                })
-                await ctx.store.save(newTokenGroupToken)
-            } else {
-                tokenGroupToken.position = index
-                await ctx.store.save(tokenGroupToken)
-            }
+    for (const tokenGroupToken of tokenGroupTokens) {
+        if (!tokenGroupToken.tokenGroup) {
+            await job.log(`Removing token group token ${tokenGroupToken.id} because it has no token group`)
+            await ctx.store.remove(tokenGroupToken)
         }
+
+        // for (const [index, tokenGroupToken] of tokenGroup.tokenGroupTokens.entries()) {
+        //     if (tokenGroupToken.id !== `${tokenGroupToken.token.tokenId}-${tokenGroup.id}`) {
+        //         const token = tokenGroupToken.token
+        //         await ctx.store.remove(tokenGroupToken)
+
+        //         const newTokenGroupToken = new TokenGroupToken({
+        //             id: `${token.tokenId}-${tokenGroup.id}`,
+        //             token,
+        //             tokenGroup,
+        //             position: index,
+        //         })
+        //         await ctx.store.save(newTokenGroupToken)
+        //     } else {
+        //         tokenGroupToken.position = index
+        //         await ctx.store.save(tokenGroupToken)
+        //     }
+        // }
     }
 
-    await job.log(`Dispatched computeTokenGroupPositions for ${tokenGroups.length} token groups`)
+    await job.log(`Dispatched computeTokenGroupPositions for ${tokenGroupTokens.length} token group tokens`)
 }
