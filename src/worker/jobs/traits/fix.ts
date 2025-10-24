809b27bde0294ecced13538954c07c826a4b5d6d
Zouheir Layine <zlayine@enjin.io>
2025-10-24 15:17:13 +0100

    add rarity fix

--- src/worker/jobs/traits/fix.ts
+++ src/worker/jobs/traits/fix.ts
@@ -1,6 +1,6 @@
 import { Job } from 'bullmq'
 import { connectionManager } from '~/contexts'
-import { TraitToken } from '~/model'
+import { TokenRarity, TraitToken } from '~/model'
 
 export async function fixTraits(job: Job, id: string) {
     const em = await connectionManager()
@@ -13,7 +13,17 @@ export async function fixTraits(job: Job, id: string) {
         },
     })
 
+    const tokenRarities = await em.find(TokenRarity, {
+        where: {
+            token: {
+                id: id,
+            },
+        },
+    })
+
     await em.remove(traitTokens)
+    await em.remove(tokenRarities)
 
     await em.query(`DELETE FROM trait_token WHERE token_id = $1`, [id])
+    await em.query(`DELETE FROM token_rarity WHERE token_id = $1`, [id])
 }
