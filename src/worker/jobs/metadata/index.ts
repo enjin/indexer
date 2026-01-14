5cb7639ae4b859907252a878ba8bad43fb11c57c
Zouheir Layine <zlayine@enjin.io>
2026-01-14 11:30:45 +0100

    fix

--- src/worker/jobs/metadata/index.ts
+++ src/worker/jobs/metadata/index.ts
@@ -1,5 +1,5 @@
 export * from './sync-metadata'
 export * from './sync-fuel-tanks'
 export * from './compute-metadata'
-export * from "./sync-tokengroup-metadata"
-export * from "./compute-tokengroup-metadata"
+export * from './sync-tokengroup-metadata'
+export * from './compute-tokengroup-metadata'
