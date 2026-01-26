738e2750bab62765ad89b0d7c5751e4ad0921ca4
Pawel Wankiewicz <pawel@enjin.io>
2026-01-26 12:11:15 +0100

    NFTIO-3606

--- src/model/overridden/eraReward.model.ts
+++ src/model/overridden/eraReward.model.ts
@@ -1,6 +1,8 @@
-import { ManyToOne as ManyToOne_, Index as Index_ } from '@subsquid/typeorm-store'
+import { ManyToOne as ManyToOne_, Index as Index_, Entity as Entity_ } from '@subsquid/typeorm-store'
 import { NominationPool } from './nominationPool.model'
 import { EraReward as EraRewardGenerated } from '~/model'
+
+@Entity_()
 export class EraReward extends EraRewardGenerated {
     @Index_()
     @ManyToOne_(() => NominationPool, { nullable: true })
--- src/model/overridden/nominationPool.model.ts
+++ src/model/overridden/nominationPool.model.ts
@@ -1,5 +1,7 @@
 import { NominationPool as NominationPoolGenerated, PoolState } from '~/model'
+import { Entity as Entity_ } from "@subsquid/typeorm-store";
 
+@Entity_()
 export class NominationPool extends NominationPoolGenerated {
     isOpen: () => boolean = (): boolean => this.state === PoolState.Open
 
