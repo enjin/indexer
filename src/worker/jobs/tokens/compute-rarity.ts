238d4d9ed7dc6bc0e555f7f4d50fc7e54a50e701
Zouheir Layine <zlayine@enjin.io>
2026-02-03 11:06:26 +0100

    disable computing

--- src/worker/jobs/tokens/compute-rarity.ts
+++ src/worker/jobs/tokens/compute-rarity.ts
@@ -41,60 +41,60 @@ export async function computeRarity(job: Job, id: string) {
     const em = await connectionManager()
 
     try {
-        const [collection, tokens] = await Promise.all([
-            em.findOneOrFail(Collection, {
-                relations: {
-                    traits: true,
-                },
-                where: { id },
-            }),
-            em.find(Token, {
-                relations: {
-                    traits: {
-                        trait: true,
-                    },
-                },
-                where: { collection: { id } },
-            }),
-        ])
+        // const [collection, tokens] = await Promise.all([
+        //     em.findOneOrFail(Collection, {
+        //         relations: {
+        //             traits: true,
+        //         },
+        //         where: { id },
+        //     }),
+        //     em.find(Token, {
+        //         relations: {
+        //             traits: {
+        //                 trait: true,
+        //             },
+        //         },
+        //         where: { collection: { id } },
+        //     }),
+        // ])
 
-        const totalSupply = collection.stats?.supply ?? 0
-        if (!totalSupply || totalSupply <= 0) {
-            return
-        }
-        const entropy = informationContentScoring.collectionEntropy(totalSupply, collection.traits)
-        if (!entropy || collection.traits.length === 0) {
-            return
-        }
+        // const totalSupply = collection.stats?.supply ?? 0
+        // if (!totalSupply || totalSupply <= 0) {
+        //     return
+        // }
+        // const entropy = informationContentScoring.collectionEntropy(totalSupply, collection.traits)
+        // if (!entropy || collection.traits.length === 0) {
+        //     return
+        // }
 
-        const tokenRarities = tokens.map((token) => {
-            return { score: informationContentScoring.scoreToken(totalSupply, entropy, token), token }
-        })
+        // const tokenRarities = tokens.map((token) => {
+        //     return { score: informationContentScoring.scoreToken(totalSupply, entropy, token), token }
+        // })
 
-        tokenRarities.sort((a, b) => Number(mathjs.compare(b.score, a.score)))
+        // tokenRarities.sort((a, b) => Number(mathjs.compare(b.score, a.score)))
 
-        tokenRarities.sort((a, b) => {
-            if (Number(mathjs.compare(a.score, b.score)) === 0) {
-                return Number(mathjs.compare(mathjs.bignumber(a.token.tokenId), mathjs.bignumber(b.token.tokenId)))
-            }
-            return 0
-        })
+        // tokenRarities.sort((a, b) => {
+        //     if (Number(mathjs.compare(a.score, b.score)) === 0) {
+        //         return Number(mathjs.compare(mathjs.bignumber(a.token.tokenId), mathjs.bignumber(b.token.tokenId)))
+        //     }
+        //     return 0
+        // })
 
-        const tokenRanks = tokenRarities.map((tokenRarity, index) => {
-            return new TokenRarity({
-                id: tokenRarity.token.id,
-                collection,
-                token: tokenRarity.token,
-                score: tokenRarity.score.toNumber(),
-                rank: index + 1,
-            })
-        })
+        // const tokenRanks = tokenRarities.map((tokenRarity, index) => {
+        //     return new TokenRarity({
+        //         id: tokenRarity.token.id,
+        //         collection,
+        //         token: tokenRarity.token,
+        //         score: tokenRarity.score.toNumber(),
+        //         rank: index + 1,
+        //     })
+        // })
 
-        await job.log(`Token ranks: ${tokenRanks.length}`)
+        // await job.log(`Token ranks: ${tokenRanks.length}`)
 
-        await em.delete(TokenRarity, { collection: { id } })
+        // await em.delete(TokenRarity, { collection: { id } })
 
-        await em.save(tokenRanks, { chunk: 1000 })
+        // await em.save(tokenRanks, { chunk: 1000 })
     } catch (error) {
         await job.log(`Error in rarity ranker ${id} ${error}`)
         return
--- src/worker/jobs/traits/compute-traits.ts
+++ src/worker/jobs/traits/compute-traits.ts
@@ -10,102 +10,102 @@ type TraitValueMap = Map<string, bigint>
 export async function computeTraits(job: Job, id: string) {
     const em = await connectionManager()
 
-    const traitTypeMap = new Map<string, TraitValueMap>()
-    const tokenTraitMap = new Map<string, string[]>()
+    // const traitTypeMap = new Map<string, TraitValueMap>()
+    // const tokenTraitMap = new Map<string, string[]>()
 
-    const tokens = await em
-        .getRepository(Token)
-        .createQueryBuilder('token')
-        .select('token.id')
-        .addSelect('token.metadata')
-        .addSelect('token.supply')
-        .where('token.collection = :id', { id })
-        .andWhere('token.supply > 0')
-        .getMany()
+    // const tokens = await em
+    //     .getRepository(Token)
+    //     .createQueryBuilder('token')
+    //     .select('token.id')
+    //     .addSelect('token.metadata')
+    //     .addSelect('token.supply')
+    //     .where('token.collection = :id', { id })
+    //     .andWhere('token.supply > 0')
+    //     .getMany()
 
-    await em.query(
-        `DELETE FROM trait_token USING trait WHERE trait.id = trait_token.trait_id AND trait.collection_id = $1`,
-        [id]
-    )
+    // await em.query(
+    //     `DELETE FROM trait_token USING trait WHERE trait.id = trait_token.trait_id AND trait.collection_id = $1`,
+    //     [id]
+    // )
 
-    await em.query(`DELETE FROM trait WHERE collection_id = $1`, [id])
+    // await em.query(`DELETE FROM trait WHERE collection_id = $1`, [id])
 
-    tokens.forEach((token) => {
-        if (!token.metadata || !token.metadata.attributes || !isPlainObject(token.metadata.attributes)) return
-        const attributes = token.metadata.attributes as Record<
-            string,
-            { value: string; display_name?: string } | string
-        >
-        Object.entries(attributes).forEach(([traitType, data]) => {
-            let value = data as string
-            if (typeof data === 'object') {
-                value = data.value
-                if (data.display_name) {
-                    traitType = data.display_name
-                }
-            }
+    // tokens.forEach((token) => {
+    //     if (!token.metadata || !token.metadata.attributes || !isPlainObject(token.metadata.attributes)) return
+    //     const attributes = token.metadata.attributes as Record<
+    //         string,
+    //         { value: string; display_name?: string } | string
+    //     >
+    //     Object.entries(attributes).forEach(([traitType, data]) => {
+    //         let value = data as string
+    //         if (typeof data === 'object') {
+    //             value = data.value
+    //             if (data.display_name) {
+    //                 traitType = data.display_name
+    //             }
+    //         }
 
-            if (!value) return
+    //         if (!value) return
 
-            value = value.toString()
+    //         value = value.toString()
 
-            if (!traitTypeMap.has(traitType)) {
-                traitTypeMap.set(traitType, new Map())
-            }
-            const tType = traitTypeMap.get(traitType) as TraitValueMap
-            if (tType.has(value)) {
-                tType.set(value, (tType.get(value) as bigint) + token.supply)
-            } else {
-                tType.set(value, token.supply)
-            }
+    //         if (!traitTypeMap.has(traitType)) {
+    //             traitTypeMap.set(traitType, new Map())
+    //         }
+    //         const tType = traitTypeMap.get(traitType) as TraitValueMap
+    //         if (tType.has(value)) {
+    //             tType.set(value, (tType.get(value) as bigint) + token.supply)
+    //         } else {
+    //             tType.set(value, token.supply)
+    //         }
 
-            tokenTraitMap.set(token.id, [...(tokenTraitMap.get(token.id) || []), hash(`${id}-${traitType}-${value}`)])
-        })
-    })
+    //         tokenTraitMap.set(token.id, [...(tokenTraitMap.get(token.id) || []), hash(`${id}-${traitType}-${value}`)])
+    //     })
+    // })
 
-    if (!traitTypeMap.size) {
-        await job.log(`No traits found for collection ${id}`)
-        return
-    }
+    // if (!traitTypeMap.size) {
+    //     await job.log(`No traits found for collection ${id}`)
+    //     return
+    // }
 
-    await job.log(`Found ${traitTypeMap.size} trait types`)
-    const traitsToSave: Trait[] = []
+    // await job.log(`Found ${traitTypeMap.size} trait types`)
+    // const traitsToSave: Trait[] = []
 
-    traitTypeMap.forEach((traitValueMap, traitType) => {
-        traitValueMap.forEach((count, value) => {
-            traitsToSave.push(
-                new Trait({
-                    id: hash(`${id}-${traitType}-${value}`),
-                    collection: new Collection({ id: id }),
-                    traitType,
-                    value,
-                    count,
-                })
-            )
-        })
-    })
+    // traitTypeMap.forEach((traitValueMap, traitType) => {
+    //     traitValueMap.forEach((count, value) => {
+    //         traitsToSave.push(
+    //             new Trait({
+    //                 id: hash(`${id}-${traitType}-${value}`),
+    //                 collection: new Collection({ id: id }),
+    //                 traitType,
+    //                 value,
+    //                 count,
+    //             })
+    //         )
+    //     })
+    // })
 
-    await job.log(`Saving ${traitsToSave.length} traits`)
-    await em.save(Trait, traitsToSave, { chunk: 1000 })
-    const traitTokensToSave: TraitToken[] = []
+    // await job.log(`Saving ${traitsToSave.length} traits`)
+    // await em.save(Trait, traitsToSave, { chunk: 1000 })
+    // const traitTokensToSave: TraitToken[] = []
 
-    tokenTraitMap.forEach((traits, tokenId) => {
-        if (!traits.length) return
-        traits.forEach((trait) => {
-            traitTokensToSave.push(
-                new TraitToken({
-                    id: hash(`${trait}-${tokenId}`),
-                    trait: new Trait({ id: trait }),
-                    token: new Token({ id: tokenId }),
-                })
-            )
-        })
-    })
+    // tokenTraitMap.forEach((traits, tokenId) => {
+    //     if (!traits.length) return
+    //     traits.forEach((trait) => {
+    //         traitTokensToSave.push(
+    //             new TraitToken({
+    //                 id: hash(`${trait}-${tokenId}`),
+    //                 trait: new Trait({ id: trait }),
+    //                 token: new Token({ id: tokenId }),
+    //             })
+    //         )
+    //     })
+    // })
 
-    if (traitTokensToSave.length) {
-        await job.log(`Saving ${traitsToSave.length} token traits`)
-        await em.save(TraitToken, traitTokensToSave, { chunk: 1000 })
-    }
+    // if (traitTokensToSave.length) {
+    //     await job.log(`Saving ${traitsToSave.length} token traits`)
+    //     await em.save(TraitToken, traitTokensToSave, { chunk: 1000 })
+    // }
 
     // delay to avoid rollback issue on fork
     QueueUtils.dispatchComputeRarity({ id, delay: 120000 })
