3897aac146011bea689a97f643fd99cfb2c06551
Zouheir Layine <zlayine@enjin.io>
2026-02-27 11:08:35 +0000

    Update src/server-extension/refresh-metadata.ts
    Co-authored-by: Pawel <pawel@enjin.io>

--- src/server-extension/refresh-metadata.ts
+++ src/server-extension/refresh-metadata.ts
@@ -140,7 +140,7 @@ export class RefreshMetadataResolver {
                     id: resource.id,
                     type: item.type === RefreshMetadataType.TOKEN ? 'token' : 'collection',
                     force: true,
-                    allTokens: item.type === RefreshMetadataType.COLLECTION ? true : false,
+                    allTokens: item.type === RefreshMetadataType.COLLECTION,
                     traits: true,
                 })
 
