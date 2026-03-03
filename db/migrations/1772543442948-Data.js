5110b753606389f6f784012818d5ba65ae282bc8
Zouheir Layine <zlayine@enjin.io>
2026-03-03 13:31:30 +0000

    fix

--- db/migrations/1772543442948-Data.js
+++ db/migrations/1772543442948-Data.js
@@ -3,11 +3,9 @@ module.exports = class Data1772543442948 {
 
     async up(db) {
         await db.query(`ALTER TABLE "trait" ADD "display_type" text`)
-        await db.query(`ALTER TABLE "trait" ADD "display_value" text`)
     }
 
     async down(db) {
         await db.query(`ALTER TABLE "trait" DROP COLUMN "display_type"`)
-        await db.query(`ALTER TABLE "trait" DROP COLUMN "display_value"`)
     }
 }
