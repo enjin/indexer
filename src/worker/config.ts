6293cc8f1991d28bd165a0a71dd0e002a4294038
Zouheir Layine <zlayine@enjin.io>
2026-03-08 15:35:00 +0000

    fix

--- src/worker/config.ts
+++ src/worker/config.ts
@@ -5,7 +5,6 @@ export const connection: ConnectionOptions = {
     host: config.redis.host,
     port: config.redis.port,
     db: config.redis.db,
-    maxRetriesPerRequest: 2,
 }
 
 export default {
