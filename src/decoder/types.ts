d20dadf501d541931f200ca997477fe08842e232
Zouheir Layine <zlayine@enjin.io>
2026-03-18 21:15:38 +0000

    add hash to signed extrinsic response

--- src/decoder/types.ts
+++ src/decoder/types.ts
@@ -77,6 +77,7 @@ export interface DecodedSignedExtrinsicInput {
 }
 
 export interface DecodedSignedExtrinsicResult {
+    hash: string
     signer: string | null
     nonce: number | null
     encodedData: string | null
