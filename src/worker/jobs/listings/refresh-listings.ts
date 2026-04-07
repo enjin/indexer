955e7cc2bba3b66e4126f11852d64dd428face39
Zouheir Layine <zlayine@enjin.io>
2026-04-07 12:25:38 +0100

    remove logs

--- src/worker/jobs/listings/refresh-listings.ts
+++ src/worker/jobs/listings/refresh-listings.ts
@@ -30,17 +30,7 @@ async function computeListingDistribution(listingData: any, assetRoyalty: bigint
     const amount = BigInt(codec.amount)
     const bigRoyalty = (assetRoyalty * BigInt(10 ** 9) * price) / BigInt(10 ** 18)
 
-    await job.log(`Price: ${price}`)
-    await job.log(`Amount: ${amount}`)
-    await job.log(`Big royalty: ${bigRoyalty}`)
-
     const protocolFee = (BigInt(0.025 * 10 ** 18) * price) / BigInt(10 ** 18)
-
-    await job.log(`Protocol fee: ${protocolFee}`)
-
-    await job.log(`Total: ${price * amount - protocolFee - bigRoyalty}`)
-    await job.log(`Min received: ${minReceived}`)
-    await job.log(`Total < min received: ${price * amount - protocolFee - bigRoyalty < minReceived}`)
 
     return Promise.resolve(price * amount - protocolFee - bigRoyalty < minReceived ? true : false)
 }
@@ -94,16 +84,11 @@ export async function refreshListings(job: Job, ids: string[]) {
                       ?.reduce((acc: number, curr: number | undefined) => acc + (curr ?? 0), 0) ?? 0)
                 : 0
 
-        await job.log(`Collection royalty total: ${collectionRoyaltyTotal}`)
-        await job.log(`Token royalty total: ${tokenRoyaltyTotal}`)
-        await job.log(`Total royalty: ${collectionRoyaltyTotal + tokenRoyaltyTotal}`)
-
         if (await computeListingDistribution(listingData, BigInt(collectionRoyaltyTotal + tokenRoyaltyTotal), job)) {
             await job.log(`Listing ${listing.id} has royalty increased`)
             if (listing.isActive) {
                 listing.hasRoyaltyIncreased = true
                 await em.save(listing)
-                await job.log(`Increased royalty for listing ${listing.id}`)
             }
             processed++
             const progress = Math.min(90, 30 + Math.floor((processed / totalListings) * 60))
