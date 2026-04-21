35641c909acb3ff6e0eea80dd93d328548f66d6e
Zouheir Layine <zlayine@enjin.io>
2026-04-21 12:18:21 +0100

    [NFTIO-3728] add decimal count to token history (#1870)
    add decimal count to token history

--- src/server-extension/token-sales-history.ts
+++ src/server-extension/token-sales-history.ts
@@ -1,7 +1,7 @@
-import { Field, ObjectType, Query, Resolver, Arg } from 'type-graphql'
+import { Field, Int, ObjectType, Query, Resolver, Arg } from 'type-graphql'
 import 'reflect-metadata'
 import type { EntityManager } from 'typeorm'
-import { Listing, ListingSale } from '~/model'
+import { Listing, ListingSale, Token } from '~/model'
 
 @ObjectType()
 export class TokenSale {
@@ -16,6 +16,9 @@ export class TokenSale {
 
     @Field({ nullable: false })
     day!: Date
+
+    @Field(() => Int, { nullable: true })
+    decimalCount!: number | null
 
     constructor(props: Partial<TokenSale>) {
         Object.assign(this, props)
@@ -37,11 +40,17 @@ export class TokenSalesHistoryResolver {
             .getRepository(ListingSale)
             .createQueryBuilder('listing_sale')
             .leftJoinAndMapOne('listing_sale.listing', Listing, 'listing', 'listing_sale.listing_id = listing.id')
+            .leftJoin(Token, 'make_token', 'make_token.id = listing.make_asset_id_id')
+            .leftJoin(Token, 'take_token', 'take_token.id = listing.take_asset_id_id')
             .where('(listing.make_asset_id_id = :id OR listing.take_asset_id_id = :id)', { id })
             .select('DATE(listing_sale.created_at) AS day')
             .addSelect('COUNT(*) AS trades')
             .addSelect('SUM(listing_sale.price) AS price')
             .addSelect('SUM(listing_sale.amount) AS amount')
+            .addSelect(
+                `MAX(CASE WHEN listing.make_asset_id_id = :id THEN (make_token.native_metadata ->> 'decimalCount')::int ELSE (take_token.native_metadata ->> 'decimalCount')::int END)`,
+                'decimalCount'
+            )
 
         if (fromDate) {
             builder.andWhere('listing_sale.created_at >= :fromDate', { fromDate })
