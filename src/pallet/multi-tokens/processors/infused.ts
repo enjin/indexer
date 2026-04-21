e7747092f9bf9f4a8e8ec3601316f00d31cc028d
Pawel <pawel@enjin.io>
2026-04-21 13:13:39 +0200

    [NFTIO-3737] Updates SNS infusion event to return public key (#1869)
    * NFTIO-3737 Updates SNS infusion event to return public key

--- src/pallet/multi-tokens/processors/infused.ts
+++ src/pallet/multi-tokens/processors/infused.ts
@@ -98,6 +98,7 @@ export async function infused(
             token: `${data.collectionId}-${data.tokenId}`,
             amount: data.amount ?? data.totalAmount,
             accountId: data.accountId,
+            account: account.id,
             extrinsic: item.extrinsic?.id,
             decimalCount: token.nativeMetadata?.decimalCount ?? 0,
         },
