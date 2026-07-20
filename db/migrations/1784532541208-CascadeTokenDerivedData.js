b31e12653127062f91ad7512782b0c0016eb2dc6
Zouheir Layine <zlayine@enjin.io>
2026-07-20 08:58:31 +0100

    [NFTIO-3907] fix token rarite/trait relations (#1916)
    fix token rarite/trait relations

---
+++ db/migrations/1784532541208-CascadeTokenDerivedData.js
@@ -0,0 +1,33 @@
+module.exports = class CascadeTokenDerivedData1784532541208 {
+    name = 'CascadeTokenDerivedData1784532541208'
+
+    async up(db) {
+        await db.query(
+            `ALTER TABLE "token_rarity" DROP CONSTRAINT "FK_a708c8b728c92f895762b5d0278"`
+        )
+        await db.query(
+            `ALTER TABLE "token_rarity" ADD CONSTRAINT "FK_a708c8b728c92f895762b5d0278" FOREIGN KEY ("token_id") REFERENCES "token"("id") ON DELETE CASCADE ON UPDATE NO ACTION`
+        )
+        await db.query(
+            `ALTER TABLE "trait_token" DROP CONSTRAINT "FK_42c1cb4414e0240cf7e0ee8f968"`
+        )
+        await db.query(
+            `ALTER TABLE "trait_token" ADD CONSTRAINT "FK_42c1cb4414e0240cf7e0ee8f968" FOREIGN KEY ("token_id") REFERENCES "token"("id") ON DELETE CASCADE ON UPDATE NO ACTION`
+        )
+    }
+
+    async down(db) {
+        await db.query(
+            `ALTER TABLE "trait_token" DROP CONSTRAINT "FK_42c1cb4414e0240cf7e0ee8f968"`
+        )
+        await db.query(
+            `ALTER TABLE "trait_token" ADD CONSTRAINT "FK_42c1cb4414e0240cf7e0ee8f968" FOREIGN KEY ("token_id") REFERENCES "token"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        )
+        await db.query(
+            `ALTER TABLE "token_rarity" DROP CONSTRAINT "FK_a708c8b728c92f895762b5d0278"`
+        )
+        await db.query(
+            `ALTER TABLE "token_rarity" ADD CONSTRAINT "FK_a708c8b728c92f895762b5d0278" FOREIGN KEY ("token_id") REFERENCES "token"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        )
+    }
+}
