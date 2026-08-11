fb22c66ac0f02a4778e9f1bd35fb4831d9d4e431
Zouheir Layine <zlayine@enjin.io>
2026-08-11 10:11:52 +0100

    fix

--- src/decoder/view/builder.ts
+++ src/decoder/view/builder.ts
@@ -69,7 +69,7 @@ function getCallSubtitle(call: CallParts, fields: ViewField[]): string {
     }
 
     const amount = CALL_AMOUNT_PATHS[callId]
-        ?.map((path) => getArg(call.params, path))
+        .map((path) => getArg(call.params, path))
         .find((value) => value !== undefined)
     if ((typeof amount === 'string' || typeof amount === 'bigint') && /^\d+$/.test(amount.toString())) {
         return `x ${amount}`
